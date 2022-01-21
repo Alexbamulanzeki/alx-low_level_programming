3e01656046c4cf144891a02468e8dcefbec693ac
Alexbamulanzeki <syliviakimuli2@gmail.com>
2022-01-20 23:27:56 -0800

    0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,21 +9,21 @@
  **/
 int linear_search(int *array, size_t size, int value)
 {
-  unsigned int i;
-  int flag = 0;
+unsigned int i;
+int flag = 0;
 
-  if (array == NULL)
-    return (-1);
-  for (i = 0; i < size; i++)
-    {
-      printf("Value checked array[%u] = [%d]\n", i, array[i]);
-      if (array[i] == value)
-	{
-	  flag = 1;
-	  return (i);
-	}
-    }
-  if (flag != 1)
-    return (-1);
-  return (i);
+if (array == NULL)
+return (-1);
+for (i = 0; i < size; i++)
+{
+printf("Value checked array[%u] = [%d]\n", i, array[i]);
+if (array[i] == value)
+{
+flag = 1;
+return (i);
+}
+}
+if (flag != 1)
+return (-1);
+return (i);
 }
